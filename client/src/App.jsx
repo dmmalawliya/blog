60df2be33fc077c51a7a8ca5d0465762dce5771a
dmmalawliya <dmmalawliya@gmail.com>
2024-09-21 15:09:20 +0530

    create and run the server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import Projects from './pages/Projects'
 import SignUp from './pages/SignUp'
 import About from './pages/About'
 import Home from './pages/Home'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/about" element={<About/>}/>
